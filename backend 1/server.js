a9a35ba48d6dceae00fa53d19ae733dd03ff5b57
vanshgupta11 <vanshkumargupta11@gmail.com>
2025-09-23 00:08:05 +0530

    post

--- backend 1/server.js
+++ backend 1/server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
 app.listen(3000 ,()=>{
     console.log("server started at port no. 3000");
     
+})
+app.get('/',(req,res)=>{
+    res.send("heloooooooooooooo");
+})
+
+app.post('/api/cars',(req,res)=>{
+    const {name,brand} = req.body;
+    console.log(name);
+    console.log(brand);
+    res.send("car submitted sucessfully")
 })

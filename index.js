2d254e2a964b2e056588b1f9c508cd1d01ba75f5
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-01-22 20:54:05 +0530

    third

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const  app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.send("working");
+});
+
+app.listen(3000,function(){
+    console.log('running')
+})

9e9c070b1a36218143488b452a7d13b01ae5bfe6
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-01-22 20:58:11 +0530

    fourth

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const  app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
 
 app.get("/",function(req,res){
     res.send("working");

c2f9e6e3d16e07c21e637804b7830e0c20e1e32b
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-01-29 01:54:17 +0530

    middleware

--- mini-project/app.js
+++ mini-project/app.js
@@ -11,11 +11,15 @@ const jwt = require('jsonwebtoken');
 app.set('view engine','ejs');
 
 app.use(express.json());
-app.use(express.urlencoded({extented : true}));
+app.use(express.urlencoded({extended : true}));
 app.use(cookieParser())
 
 app.get('/',(req,res)=>{
     res.render('index');
+});
+app.get('/profile',isLoggedIn,(req,res)=>{
+    console.log(req.user);
+    res.render('login')
 });
 
 app.get('/login',(req,res)=>{
@@ -52,14 +56,28 @@ app.post('/login', async (req,res)=>{
     if(!user) return res.status(500).send("something went wrong");
     
    bcrypt.compare(password , user.password , (err,result)=>{
-            if(result) return res.status(200).send("you can login");
+            if(result) {
+                let token =  jwt.sign({email , userid : user._id} , "shhh");
+                res.cookie("token",token);
+                res.status(200).send("you can login");
+            }
+
             else res.redirect("/login")
    })
 });
 
     app.get('/logout',(req,res)=>{
         res.cookie("token","");
-        res.redirect("/login")
-    })
+        res.redirect("/login");
+    });
+
+    function isLoggedIn(req,res,next){
+        if(req.cookies.token === "") res.send("you must be logged in");
+        else{
+            let data = jwt.verify(req.cookies.token ,"shhh");
+            req.user = data ;
+            next();
+        }
+    }
 
 app.listen(3000);

402936eaf1ee38c9c5fbc626449042ec0c984585
vanshgupta11 <vanshkumargupta11@gmail.com>
2026-02-05 10:29:46 +0530

    like2

--- mini-project/app.js
+++ mini-project/app.js
@@ -25,7 +25,14 @@ app.get('/profile',isLoggedIn, async (req,res)=>{
 });
 
 app.get('/like/:id',isLoggedIn, async (req,res)=>{
-    let post = await postModel.findOne({ _id: req.params.id}).populate("user"); 
+    let post = await postModel.findOne({_id: req.params.id}).populate("user"); 
+
+    if(post.likes.indexOf(req.user.userid) === -1){
+        post.likes.push(req.user.userid);
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userid),1)
+    }
     post.likes.push(req.user.userid);
     await post.save();
     res.redirect('/profile');
